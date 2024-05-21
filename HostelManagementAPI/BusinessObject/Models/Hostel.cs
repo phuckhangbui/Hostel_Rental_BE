1f072bcec4f1d83eae9c109eb19b2ee511fe0470
Minh Nguyên <108600635+Nguyennm080503@users.noreply.github.com>
2024-05-21 15:46:56 +0700

    add status hostel

--- HostelManagementAPI/BusinessObject/Models/Hostel.cs
+++ HostelManagementAPI/BusinessObject/Models/Hostel.cs
@@ -8,6 +8,7 @@
         public string? HostelDescription { get; set; }
         public int? AccountID { get; set; }
         public Account OwnerAccount { get; set; }
+        public int? Status { get; set; }
 
         public IList<Room> Rooms { get; set; }
     }
