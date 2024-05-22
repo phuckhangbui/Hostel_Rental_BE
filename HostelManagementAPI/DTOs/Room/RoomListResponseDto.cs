06c70a19da7d1bf55c138f2f6d083ba332aa5a1a
khoalnd <khoaly090141@gmail.com>
2024-05-22 19:52:11 +0700

    add api get list room by hostel, get room detail, change room status

---
+++ HostelManagementAPI/DTOs/Room/RoomListResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DTOs.Room
+{
+	public class RoomListResponseDto
+	{
+		public int RoomID { get; set; }
+		public string? RoomName { get; set; }
+		public int? Capacity { get; set; }
+		public double? RoomFee { get; set; }
+		public int? HostelID { get; set; }
+		public int Status { get; set; }
+		public string? RoomThumbnail { get; set; }
+	}
+}
--- HostelManagementAPI/Service/Interface/IRoomService.cs
+++ HostelManagementAPI/Service/Interface/IRoomService.cs
@@ -1,11 +1,14 @@
-﻿using DTOs;
+﻿using DTOs.Room;
 using Microsoft.AspNetCore.Http;
 
 namespace Service.Interface
 {
-	public interface IRoomService
+    public interface IRoomService
 	{
 		Task CreateRoom(CreateRoomRequestDto createRoomRequestDto);
 		Task UploadRoomImage(IFormFileCollection files, int roomId);
+		Task <IEnumerable<RoomListResponseDto>> GetListRoomsByHostelId(int hostelId);
+		Task <RoomDetailResponseDto> GetRoomDetailByRoomId(int roomId);
+		Task ChangeRoomStatus(int roomId, int status);
 	}
 }
