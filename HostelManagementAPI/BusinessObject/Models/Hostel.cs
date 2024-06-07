d33cd139bb216545d411854c032c86991eddeafc
HoangMinhNg <ngminh3004@gmail.com>
2024-06-07 13:02:42 +0700

    update hostel and room attribute

--- HostelManagementAPI/BusinessObject/Models/Hostel.cs
+++ HostelManagementAPI/BusinessObject/Models/Hostel.cs
@@ -7,6 +7,8 @@
         public string? HostelAddress { get; set; }
         public string? HostelDescription { get; set; }
         public int? AccountID { get; set; }
+        public string? HostelType { get; set; }
+        public DateTime? CreateDate { get; set; }
         public Account OwnerAccount { get; set; }
         public int? Status { get; set; }
         public IList<Room>? Rooms { get; set; }
