99306bc282615771706025f61ea4dd9759c581dd
AskWhyNot <109494300+askwhynot1309@users.noreply.github.com>
2024-06-11 21:35:46 +0700

    add field hostel name

---
+++ HostelManagementAPI/Dao/ContractMemberDao.cs
@@ -0,0 +1,34 @@
+﻿using BusinessObject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAO
+{
+    public class ContractMemberDao
+    {
+        private static ContractMemberDao instance = null;
+        private static readonly object instacelock = new object();
+
+        private ContractMemberDao()
+        {
+
+        }
+
+        public static ContractMemberDao Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ContractMemberDao();
+                }
+                return instance;
+            }
+        }
+
+        public async Task<IEnumerable<ContractMember>> GetContractMembersAsync()
+        {
+            var context = new DataContext();
+            return await context.ContractMember.ToListAsync();
+        }
+    }
+}
