df559742af48e300007e02187ce10949603a5401
phuckhangbui <khangnguyenphucbui@gmail.com>
2024-06-24 11:38:07 +0700

    add field to notification

---
+++ HostelManagementAPI/DAO/Migrations/20240624043734_addFieldToNotification.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAO.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFieldToNotification : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ForwardToPath",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsRead",
+                table: "Notifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForwardToPath",
+                table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "IsRead",
+                table: "Notifications");
+        }
+    }
+}
